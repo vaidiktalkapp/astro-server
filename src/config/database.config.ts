6ac1d4424bf0334bbefb857c55763bba9ad0e218
vishalyadavvv <vishalkumaryadav735@gmail.com>
2026-07-16 19:03:44 +0530

    fix

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -3,7 +3,7 @@ import { registerAs } from '@nestjs/config';
 export default registerAs('database', () => ({
   uri: process.env.MONGODB_URI || 'mongodb://localhost:27017/vaidiktalk',
   options: {
-    maxPoolSize: 10,
+    maxPoolSize: 45,
     serverSelectionTimeoutMS: 5000,
     socketTimeoutMS: 45000,
   },
